68933b3477a86974bca4ffa8c37f3b0299269718
Maximiliano Oller <117782970+maxioller@users.noreply.github.com>
2023-04-15 19:55:02 -0300

    api clima

--- js/integrador.js
+++ js/integrador.js
@@ -1,24 +1,53 @@
 const api = {
     key: '95cd4f233ae9e6fc3e3c50aa1d461e33',
-    url: 'https://api.openweathermap.org/data/2.5/weather',
-}
-
-async function search(query) {
-    try {
-        const response=await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
-        const data = await response.json();
-        console.log(data);
-    }catch(err){
-        console.log("Error");
-        alert('hubo un error');
+    url: `https://api.openweathermap.org/data/2.5/weather`
+  }
+  
+  const card = document.getElementById('card')
+  
+  const city = document.getElementById('city');
+  const tempImg = document.getElementById('temp-img');
+  const temp = document.getElementById('temp');
+  const weather = document.getElementById('weather');
+  const range = document.getElementById('range');
+  
+  function updateImages(data) {
+    const temp = toCelsius(data.main.temp);
+    let src = 'assets/images/icons-api/temp-mid.png';
+    if (temp > 26) {
+      src = 'assets/images/icons-api/temp-high.png';
+    } else if (temp < 20) {
+      src = 'assets/images/icons-api/temp-low.png';
     }
-}
-
-function onSubmit(event) {
+    tempImg.src = src;
+  }
+  
+  async function search(query) {
+    try {
+      const response = await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
+      const data = await response.json();
+      card.style.display = 'block';
+      city.innerHTML = `${data.name}, ${data.sys.country}`;
+      data.innerHTML = (new Date()).toLocaleDateString();
+      temp.innerHTML = `${toCelsius(data.main.temp)}°c`;
+      weather.innerHTML = data.weather[0].description;
+      range.innerHTML = `${toCelsius(data.main.temp_min)}°c / ${toCelsius(data.main.temp_max)}°c`;
+      updateImages(data);
+    } catch (err) {
+      console.log(err);
+      alert('Hubo un error');
+    }
+  }
+  
+  function toCelsius(kelvin) {
+    return Math.round(kelvin - 273.15);
+  }
+  
+  function onSubmit(event) {
     event.preventDefault();
     search(searchbox.value);
   }
-
-const searchform = document.getElementById('search-form');
-const searchbox = document.getElementById('searchbox');
-searchform.addEventListener('submit', onSubmit, true);
+  
+  const searchform = document.getElementById('search-form');
+  const searchbox = document.getElementById('searchbox');
+  searchform.addEventListener('submit', onSubmit, true);
